b54cfae45f2931714ef60129eee1dd2607163787
YashPatel4268 <142138437+YashPatel4268@users.noreply.github.com>
2026-04-07 17:49:54 +0530

    Implemented dynamic sitemap generation with caching, added static sitemap.xml creation and cache clearing feature

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SitemapController;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-
+// Sitemap Routes
 Route::get('/sitemap.xml', [SitemapController::class, 'index']);
+Route::get('/generate-sitemap', [SitemapController::class, 'generateFile']);
+Route::get('/clear-sitemap-cache', [SitemapController::class, 'clearCache']);
